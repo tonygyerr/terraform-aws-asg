068ffb6d6824209d79b069047a2d7295d0a3ce07
Gyepi-Garbrah, Ekow <ekow.gyepi-garbrah@accenture.com>
2021-11-20 10:22:11 -0500

    asg

--- asg.tf
+++ asg.tf
@@ -1,7 +1,7 @@
 resource "aws_autoscaling_group" "api" {
   count                     = var.vpc_config.environment == var.vpc_config.environment ? 1 : 0
   name                      = format("%s${var.app_name}-asg", local.name_prefix) #"${var.app_name}-ec2-asg-${count.index}"
-  vpc_zone_identifier       = var.lb_subnet_ids
+  vpc_zone_identifier       = var.subnet_ids
   launch_configuration      = aws_launch_configuration.api.*.name[count.index]
   desired_capacity          = var.ecs["desired_capacity"]
   health_check_grace_period = var.ecs["health_check_grace_period"]

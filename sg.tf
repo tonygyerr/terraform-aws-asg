ea5cba93b0ee0711edb81588bfdf062a44dad1a9
Gyepi-Garbrah, Ekow <ekow.gyepi-garbrah@accenture.com>
2021-11-21 18:06:00 -0500

    sg.tf

--- sg.tf
+++ sg.tf
@@ -44,3 +44,67 @@ resource "aws_security_group" "asg" {
     cidr_blocks = [var.open_cidr]
   }
 }
+
+resource "aws_security_group_rule" "http"{
+  type = "ingress"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  security_group_id = aws_security_group.asg.id
+  source_security_group_id = aws_security_group.asg.id
+}
+
+resource "aws_security_group_rule" "django"{
+  type = "ingress"
+  from_port   = 8000
+  to_port     = 8000
+  protocol    = "tcp"
+  security_group_id = aws_security_group.asg.id
+  source_security_group_id = aws_security_group.asg.id
+}
+
+resource "aws_security_group_rule" "web"{
+  type = "ingress"
+  from_port   = 8001
+  to_port     = 8001
+  protocol    = "tcp"
+  security_group_id = aws_security_group.asg.id
+  source_security_group_id = aws_security_group.asg.id
+}
+
+resource "aws_security_group_rule" "web_https"{
+  type = "ingress"
+  from_port   = 8002
+  to_port     = 8002
+  protocol    = "tcp"
+  security_group_id = aws_security_group.asg.id
+  source_security_group_id = aws_security_group.asg.id
+}
+
+resource "aws_security_group_rule" "sqs"{
+  type = "ingress"
+  from_port   = 9324
+  to_port     = 9324
+  protocol    = "tcp"
+  security_group_id = aws_security_group.asg.id
+  source_security_group_id = aws_security_group.asg.id
+}
+
+resource "aws_security_group_rule" "https"{
+  type = "ingress"
+  from_port   = 443
+  to_port     = 443 
+  protocol    = "tcp"
+  security_group_id = aws_security_group.asg.id
+  source_security_group_id = aws_security_group.asg.id
+}
+
+resource "aws_security_group_rule" "egress" {
+  description       = "Allow all egress traffic"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.asg.id
+  type              = "egress"
+}

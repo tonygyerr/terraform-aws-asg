90da1cff12f84dab5bfe6d60b160faf5723edf6b
Gyepi-Garbrah, Ekow <ekow.gyepi-garbrah@accenture.com>
2021-11-21 18:29:26 -0500

    sg.tf

--- sg.tf
+++ sg.tf
@@ -99,12 +99,11 @@ resource "aws_security_group_rule" "https"{
   source_security_group_id = aws_security_group.asg.id
 }
 
-resource "aws_security_group_rule" "egress" {
-  description       = "Allow all egress traffic"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
+resource "aws_security_group_rule" "vpc"{
+  type = "ingress"
+  from_port   = 0
+  to_port     = 65535
+  protocol    = "tcp"
   security_group_id = aws_security_group.asg.id
-  type              = "egress"
+  source_security_group_id = aws_security_group.asg.id
 }

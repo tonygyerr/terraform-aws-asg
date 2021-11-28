92b8c17769faae9295f0cc7b93ab5c357c642aa9
Gyepi-Garbrah, Ekow <ekow.gyepi-garbrah@accenture.com>
2021-11-27 21:38:16 -0500

    sg.tf

--- sg.tf
+++ sg.tf
@@ -45,65 +45,47 @@ resource "aws_security_group" "asg" {
   }
 }
 
-resource "aws_security_group_rule" "http"{
-  type = "ingress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  security_group_id = aws_security_group.asg.id
-  source_security_group_id = aws_security_group.asg.id
-}
+# resource "aws_security_group_rule" "http"{
+#   type = "ingress"
+#   from_port   = 80
+#   to_port     = 80
+#   protocol    = "tcp"
+#   security_group_id = aws_security_group.asg.id
+#   source_security_group_id = aws_security_group.asg.id
+# }
 
-resource "aws_security_group_rule" "django"{
-  type = "ingress"
-  from_port   = 8000
-  to_port     = 8000
-  protocol    = "tcp"
-  security_group_id = aws_security_group.asg.id
-  source_security_group_id = aws_security_group.asg.id
-}
+# resource "aws_security_group_rule" "django"{
+#   type = "ingress"
+#   from_port   = 8000
+#   to_port     = 8000
+#   protocol    = "tcp"
+#   security_group_id = aws_security_group.asg.id
+#   source_security_group_id = aws_security_group.asg.id
+# }
 
-resource "aws_security_group_rule" "web"{
-  type = "ingress"
-  from_port   = 8001
-  to_port     = 8001
-  protocol    = "tcp"
-  security_group_id = aws_security_group.asg.id
-  source_security_group_id = aws_security_group.asg.id
-}
+# resource "aws_security_group_rule" "sqs"{
+#   type = "ingress"
+#   from_port   = 9324
+#   to_port     = 9324
+#   protocol    = "tcp"
+#   security_group_id = aws_security_group.asg.id
+#   source_security_group_id = aws_security_group.asg.id
+# }
 
-resource "aws_security_group_rule" "web_https"{
-  type = "ingress"
-  from_port   = 8002
-  to_port     = 8002
-  protocol    = "tcp"
-  security_group_id = aws_security_group.asg.id
-  source_security_group_id = aws_security_group.asg.id
-}
+# resource "aws_security_group_rule" "https"{
+#   type = "ingress"
+#   from_port   = 443
+#   to_port     = 443 
+#   protocol    = "tcp"
+#   security_group_id = aws_security_group.asg.id
+#   source_security_group_id = aws_security_group.asg.id
+# }
 
-resource "aws_security_group_rule" "sqs"{
-  type = "ingress"
-  from_port   = 9324
-  to_port     = 9324
-  protocol    = "tcp"
-  security_group_id = aws_security_group.asg.id
-  source_security_group_id = aws_security_group.asg.id
-}
-
-resource "aws_security_group_rule" "https"{
-  type = "ingress"
-  from_port   = 443
-  to_port     = 443 
-  protocol    = "tcp"
-  security_group_id = aws_security_group.asg.id
-  source_security_group_id = aws_security_group.asg.id
-}
-
-resource "aws_security_group_rule" "vpc"{
-  type = "ingress"
-  from_port   = 0
-  to_port     = 65535
-  protocol    = "tcp"
-  security_group_id = aws_security_group.asg.id
-  source_security_group_id = aws_security_group.asg.id
-}
+# resource "aws_security_group_rule" "vpc"{
+#   type = "ingress"
+#   from_port   = 0
+#   to_port     = 65535
+#   protocol    = "tcp"
+#   security_group_id = aws_security_group.asg.id
+#   source_security_group_id = aws_security_group.asg.id
+# }
